1a9b7e095d42879535b9236c3b11f8822bf0ea18
Carlos21122000 <53351491+Carlos21122000@users.noreply.github.com>
2020-01-22 22:23:19 -0600

    Update Procesos.java

--- Procesos.java
+++ Procesos.java
@@ -4,6 +4,12 @@
  * and open the template in the editor.
  */
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  *
  * @author craxt
@@ -16,36 +22,27 @@ public class Procesos {
     //*Se inicializan los atributos.
     this.encendida = encendida;
  } 
-     public String encencerTelefono(Radio t){
-//Si el teléfono no esta encendido y el método encendido es verdadero entonces el teléfono se encenderá.
+     public String encencerRadio(Radio t){
+
  if(t.getencendido()==false){
     t.setencendido(true);
-    return "El telefono se eh encendido";
+    return "El radio se eh encendido";
  }
-//Si no entonces el teléfono ya estaba encendio.
+
     else
-    return "El telefono ya estaba encendido";
- //método para apagar el telefono
- }
- public String apagarTelefono(Radio t){
-//Si el teléfono esta encendido entonces el teléfono se apagara.
- if(t.getencendido()==(true)){
-    t.setencendido(false);
-    return "El telefono se apagó";
- }
-//Si no entonces el teléfono ya estaba apagado.
-    else
-    return "El teléfono ya estaba apagado";
- }
-//*método para realizar una llamada con la clase teléfono como parámetro.
- public String hacerllamada(Radio t){
-//Si el estado del teléfono esta encendido entonces podra realizar una llamada.
-    if(t.getencendida()==(true)){
-        return "Llamada activa";
- }
-//Si no entonces el teléfono esta apagado y deberá encenderse primero.
-    else
-    return "falta encender el telefono";
+    return "El radio ya estaba encendido";
 
  }
+ public String apagarRadio(Radio t){
+
+ if(t.getencendido()==(true)){
+    t.setencendido(false);
+    return "El radio se apagó";
+ }
+
+    else
+    return "El radio ya estaba apagado";
+ }
+} 
+
 } 
